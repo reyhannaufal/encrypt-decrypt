321dc76ea34d2db4ef21357248643f1ee40182ee
reyhannaufal <reyhannaufal21@gmail.com>
2022-10-09 10:38:57 +0700

    refactor: implement menu and command enum

--- rypc_client.py
+++ rypc_client.py
@@ -1,28 +1,58 @@
 import rpyc
 import time
+from enum import Enum
+
+class Command(Enum):
+    E_AES  = "encrypt_aes"
+    D_AES = "decrypt_aes"
+    E_DES = "encrypt_des"
+    D_DES = "decrypt_des"
+    QUIT = "quit"
+
+def print_menu():
+    print(f'{"="*20} Menu {"="*20}')
+    for i in Command:
+        print(f'{i.value}')
+    print(f'{"="*20} {"="*20}')
 
 def main():
     config = {'allow_public_attrs': True}
     proxy = rpyc.connect('localhost', 18861, config=config)
     option = " "
     while(option != "quit"):
+        print_menu()
         option=input("Input command: ")
         msg=option.split()
-        if(msg[0] == "e_aes"):
+        user_input_command = msg[0]
+        print(
+            f"Command: {user_input_command} - {Command(user_input_command).value}")
+        if(user_input_command == Command.E_AES.value):
             file_path = input("input path file [plaintext]: ")
             password = input("input password: ")
             with open(file_path, 'r') as f:
                 data_file = f.read()
             proxy.root.encrypt_AES(data_file, file_path, password)
             print("File berhasil diencrypt")
-        elif(msg[0] == "d_aes"):
+        elif(user_input_command == Command.D_AES.value):
             file_path = input("input path file [ciphertext]: ")
             password = input("input password: ")
             with open(file_path, 'rb') as f:
                 data_file = f.read()
             proxy.root.decrypt_AES(data_file, file_path, password)
             print("File berhasil diencrypt")
-        elif(msg[0] == "quit"):
+        elif(user_input_command == Command.E_DES.value):
+            file_path = input("input path file [plaintext]: ")
+            with open(file_path, 'r') as f:
+                data_file = f.read()
+            proxy.root.encrypt_DES(data_file, file_path)
+            print("File berhasil diencrypt")
+        elif(user_input_command == Command.D_DES.value):
+            file_path = input("input path file [ciphertext]: ")
+            with open(file_path, 'rb') as f:
+                data_file = f.read()
+            proxy.root.decrypt_DES(data_file, file_path)
+            print("File berhasil diencrypt")
+        elif(user_input_command == Command.QUIT.value):
             time.sleep(2)
             proxy.close()
         else:
--- rypc_server.py
+++ rypc_server.py
@@ -4,6 +4,12 @@ import base64
 import hashlib
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
+from Crypto.Cipher import DES
+from secrets import token_bytes
+
+_key_des__ = token_bytes(8)
+__key_aes__ = None
+
 
 
 def encrypt_AES(raw):
@@ -12,19 +18,36 @@ def encrypt_AES(raw):
 
     raw = base64.b64encode(pad(raw).encode('utf8'))
     iv = get_random_bytes(AES.block_size)
-    cipher = AES.new(mode= AES.MODE_CFB,iv= iv, key=__key__)
+    cipher = AES.new(mode= AES.MODE_CFB,iv= iv, key=__key_aes__)
     return base64.b64encode(iv + cipher.encrypt(raw))
 
 def decrypt_AES(enc):
     enc = base64.b64decode(enc)
     iv = enc[:AES.block_size]
-    cipher = AES.new(mode= AES.MODE_CFB,iv= iv, key=__key__)
+    cipher = AES.new(mode= AES.MODE_CFB,iv= iv, key=__key_aes__)
     return base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf8').rstrip(chr(AES.block_size))
 
 # generate key
 def generate_key(password):
-    global __key__
-    __key__ = hashlib.sha256(password.encode()).digest()
+    __key_aes__ = hashlib.sha256(password.encode()).digest()
+
+
+def encrypt_DES(msg):
+    cipher = DES.new(_key_des__, DES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
+    return nonce, ciphertext, tag
+
+
+def decrypt_DES(nonce, ciphertext, tag):
+    cipher = DES.new(_key_des__, DES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+
+    try:
+        cipher.verify(tag)
+        return plaintext.decode('ascii')
+    except:
+        return False
 
 
 class SscretMessageService(rpyc.Service):
@@ -33,12 +56,26 @@ class SscretMessageService(rpyc.Service):
        ciphertext = encrypt_AES(plain_text)
        with open(file_path, 'wb') as f:
             f.write(ciphertext)
-       print(__key__)
        print("File encrypted [server]")
     def exposed_decrypt_AES(self, cipher_text, file_path, password):
         __key__ = password
         cipher_text = cipher_text.decode('utf-8')
         plaintext = decrypt_AES(cipher_text)
+        with open(file_path, 'w') as f:
+            f.write(plaintext)
+        print("File decrypted [server]")
+    def exposed_encrypt_DES(self, plain_text, file_path):
+        nonce, ciphertext, tag = encrypt_DES(plain_text)
+        with open(file_path, 'wb') as f:
+            f.write(nonce)
+            f.write(ciphertext)
+            f.write(tag)
+        print("File encrypted [server]")
+    def exposed_decrypt_DES(self, cipher_text, file_path):
+        nonce = cipher_text[:8]
+        ciphertext = cipher_text[8:-16]
+        tag = cipher_text[-16:]
+        plaintext = decrypt_DES(nonce, ciphertext, tag)
         with open(file_path, 'w') as f:
             f.write(plaintext)
         print("File decrypted [server]")
