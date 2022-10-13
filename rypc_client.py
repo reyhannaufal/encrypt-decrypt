9b3cf138fa7ae510958014a0f65541518044722d
ajoedhi19 <74102918+ajoedhi19@users.noreply.github.com>
2022-10-14 03:22:51 +0700

    fix encrypt image and video

--- rypc_client.py
+++ rypc_client.py
@@ -1,6 +1,8 @@
 import rpyc
 import time
 from enum import Enum
+from pathlib import Path
+import base64
 
 class Command(Enum):
     E_AES  = "encrypt_aes"
@@ -24,56 +26,88 @@ def print_menu():
         print(f'{i.value}')
     print(f'{"="*20} {"="*20}')
 
+def is_not_file(file_path):
+    if Path(file_path).is_file():
+        return False
+    else:
+        print("AYYO there's no such file yo!\n==========================\n")
+        return True
+
 def user_input_handler(proxy, user_input_command):
     print(
             f"Command: {user_input_command} - {Command(user_input_command).value}")
     if(user_input_command == Command.E_AES.value):
         file_path = input(TEXT_INPUT.PLAINTEXT.value)
+        if is_not_file(file_path):
+            return
         password = input(TEXT_INPUT.PASSWORD.value)
-        with open(file_path, 'r') as f:
-            data_file = f.read()
+        data_file = None
+        if file_path.endswith('.txt'):
+            with open(file_path, 'r') as f:
+                data_file = f.read()
         proxy.root.encrypt_AES(data_file, file_path, password)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.D_AES.value):
         file_path = input(TEXT_INPUT.CIPHERTEXT.value)
+        if is_not_file(file_path):
+            return
         password = input(TEXT_INPUT.PASSWORD.value)
-        with open(file_path, 'rb') as f:
-            data_file = f.read()
+        data_file = None
+        if file_path.endswith('.txt'):
+            with open(file_path, 'r') as f:
+                data_file = f.read()
         proxy.root.decrypt_AES(data_file, file_path, password)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.E_DES.value):
         file_path = input(TEXT_INPUT.PLAINTEXT.value)
+        if is_not_file(file_path):
+            return
         with open(file_path, 'r') as f:
             data_file = f.read()
         proxy.root.encrypt_DES(data_file, file_path)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.D_DES.value):
         file_path = input(TEXT_INPUT.CIPHERTEXT.value)
+        if is_not_file(file_path):
+            return
         with open(file_path, 'rb') as f:
             data_file = f.read()
         proxy.root.decrypt_DES(data_file, file_path)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.E_RC4.value):
         file_path = input(TEXT_INPUT.CIPHERTEXT.value)
+        if is_not_file(file_path):
+            return
         password = input(TEXT_INPUT.PASSWORD.value)
-        with open(file_path, 'rb') as f:
-            data_file = f.read()
-        proxy.root.encrypt_RC4(data_file, file_path)
+        data_file = None
+        if file_path.endswith('.mp4') or file_path.endswith('.jpg') or file_path.endswith('.png'):
+            with open(file_path, 'rb') as f:
+                data_file = base64.b64encode(f.read())
+        elif file_path.endswith('.txt'):
+            with open(file_path, 'r') as f:
+                data_file = f.read()
+        proxy.root.encrypt_RC4(data_file, file_path, password)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.D_RC4.value):
         file_path = input(TEXT_INPUT.CIPHERTEXT.value)
+        if is_not_file(file_path):
+            return
         password = input(TEXT_INPUT.PASSWORD.value)
-        with open(file_path, 'rb') as f:
-            data_file = f.read()
-        proxy.root.decrypt_RC4(data_file, file_path)
+        data_file = None
+        if file_path.endswith('.enc'):
+            with open(file_path, 'r') as f:
+                data_file = f.read()
+        proxy.root.decrypt_RC4(data_file, file_path, password)
         print(TEXT_INPUT.SUCESS.value)
     elif(user_input_command == Command.QUIT.value):
         time.sleep(2)
         proxy.close()
+    
 
 def main():
     config = {'allow_public_attrs': True}
     proxy = rpyc.connect('localhost', 18861, config=config)
+    proxy._config['sync_request_timeout'] = None
     option = " "
     while(option != Command.QUIT.value):
         print_menu()
