109f2041d2d15bf335b510262192ed88f7518593
feratyusa <prabuega55@gmail.com>
2022-11-01 16:52:40 +0700

    feat: sign pdf file

--- digital-signature/digital_signature.py
+++ digital-signature/digital_signature.py
@@ -5,17 +5,38 @@ from Crypto.Signature import PKCS1_v1_5
 
 def sign_file(filename, private_key):
     h = SHA256.new()
+    signed_file = 'signed_'+filename
+
     with open(filename, 'rb') as f:
+        with open(signed_file, 'xb') as fw:
+            fw.write(f.read())
         h.update(f.read())
     signer = PKCS1_v1_5.new(private_key)
-    return signer.sign(h)
+
+    signature = signer.sign(h)
+
+    # Sign PDF file
+    with open(signed_file, 'r+b') as f:
+        f.seek(0, 2)
+        f.write(signature)
+
+    return signature
 
 def verify_file(filename, signature, public_key):
     h = SHA256.new()
+    print(filename)
     with open(filename, 'rb') as f:
         h.update(f.read())
     verifier = PKCS1_v1_5.new(public_key)
-    return verifier.verify(h, signature)
+
+    # Verify Signed PDF file
+    with open(filename, 'r+b') as f:
+        f.seek(0, 2)
+        try:
+            verifier.verify(h, signature)
+            return "The signature is valid."
+        except (ValueError, TypeError):
+            return "The signature is NOT valid."
 
 def main():
     if len(sys.argv) != 4:
@@ -26,7 +47,7 @@ def main():
     filename = sys.argv[3]
     signature = sign_file(filename, private_key)
     print('Signature: {}'.format(signature))
-    print('Verified: {}'.format(verify_file(filename, signature, public_key)))
+    print('Verified: {}'.format(verify_file('signed_'+filename, signature, public_key)))
 
 if __name__ == '__main__':
     main()
