b86c408b525a0dee15e76b49eb03aef57eed5ada
feratyusa <prabuega55@gmail.com>
2022-11-02 12:37:26 +0700

    feat: embed signature inside the pdf
    feat: seperate sign and verify method

--- digital-signature/digital_signature.py
+++ digital-signature/digital_signature.py
@@ -1,54 +1,68 @@
+import shutil
 import sys
+import os
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_v1_5
+from Crypto.Signature import pss
 
 def sign_file(filename, private_key):
     h = SHA256.new()
     signed_file = 'signed_'+filename
 
     with open(filename, 'rb') as f:
-        with open(signed_file, 'xb') as fw:
-            fw.write(f.read())
         h.update(f.read())
-    signer = PKCS1_v1_5.new(private_key)
-
+    signer = pss.new(private_key)
     signature = signer.sign(h)
 
     # Sign PDF file
-    with open(signed_file, 'r+b') as f:
-        f.seek(0, 2)
+    with open(signed_file, 'wb') as f:
+        with open(filename, 'rb') as fb:
+            f.write(fb.read())
         f.write(signature)
-
     return signature
 
-def verify_file(filename, signature, public_key):
+def verify_file(filename, public_key):
     h = SHA256.new()
-    print(filename)
-    with open(filename, 'rb') as f:
-        h.update(f.read())
-    verifier = PKCS1_v1_5.new(public_key)
+    s = b''
+    with open(filename, 'r+b') as f:
+        # Take signature at the end of file
+        f.seek(-128, 2)
+        s = f.read()
+    print(s)
+    
+    with open(filename, 'r+b') as f:
+        with open('___dummy.pdf', 'w+b') as fb:
+            shutil.copyfileobj(f, fb)
+            fb.seek(-128, 2)
+            fb.truncate()
+            # Read only the original file part
+            fb.seek(0, 0)
+            h.update(fb.read())
+    os.remove('___dummy.pdf')
 
     # Verify Signed PDF file
-    with open(filename, 'r+b') as f:
-        f.seek(0, 2)
-        try:
-            verifier.verify(h, signature)
-            return "The signature is valid."
-        except (ValueError, TypeError):
-            return "The signature is NOT valid."
+    mess=''
+    try:
+        pss.new(public_key).verify(h, s)
+        mess="The signature is valid."
+    except (ValueError, TypeError):
+        mess="The signature is NOT valid."
+    return mess
 
 def main():
-    if len(sys.argv) != 4:
-        print('Usage: digital_signature.py <private_key> <public_key> <filename>')
+    if len(sys.argv) != 4 :
+        print('Usage: digital_signature.py <sign/verify> <private_key/public_key> <filename/signed_filename>')
         return
-    private_key = RSA.importKey(open(sys.argv[1]).read())
-    public_key = RSA.importKey(open(sys.argv[2]).read())
+    key = RSA.importKey(open(sys.argv[2]).read())
     filename = sys.argv[3]
-    signature = sign_file(filename, private_key)
-    print('Signature: {}'.format(signature))
-    print('Verified: {}'.format(verify_file('signed_'+filename, signature, public_key)))
+    if sys.argv[1] == 'sign':
+        signature = sign_file(filename, key)
+        print('Signature: {}'.format(signature))
+    elif sys.argv[1] == 'verify':
+        print('Verified: {}'.format(verify_file(filename, key)))
+    else:
+        print('Usage: digital_signature.py <sign/verify> <private_key/public_key> <filename>')
 
 if __name__ == '__main__':
     main()
-
